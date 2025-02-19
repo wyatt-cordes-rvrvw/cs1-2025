a415520fd065111c67abaf7d89343b7de59247c4
wyatt-cordes-rvrvw <wyatt.cordes@rvrvw.org>
2025-02-19 19:56:54 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wyatt-cordes-rvrvw/cs1-2025
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,22 @@
+/*-------------------------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+ *-------------------------------------------------------------------------------------------------------------*/
+
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./src"))
+	http.Handle("/", fs)
+
+	log.Print("Listening on :8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
