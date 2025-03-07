88dbe4c337cf8bbf7ef80ba1910f256240f388a4
wyatt-cordes-rvrvw <wyatt.cordes@rvrvw.org>
2025-03-07 19:57:13 +0000

    added components

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,27 @@ function submit() {
     var message = document.getElementsByName("message").item(0).value;
     console.log(name);
     console.log(message);
-    alert("Hello " + name + "! your message is: " + message);
+    alert("Hello " + name + "! Your message is: " + message);
     document.getElementById("message").innerHTML = message;
 }
+
+function createNav() {
+    var nav = '<a href="/">Home</a>';
+    nav += '|<a href="/newpage/">My New Page</a>';
+    nav += '|<a href="/more/">More Pages</a>';
+    document.getElementById("topnav").innerHTML = nav;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    createNav();
+});
+
+ class anchor {
+    constructor(name, url) {
+        this.name = name;
+        this.url = url;
+    }
+    toHTML() {
+        return '<a href="' + this.url + '">' + this.name + '</a>';
+    }
+ }
